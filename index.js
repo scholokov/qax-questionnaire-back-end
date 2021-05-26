fd1fcad0162bade93799628d1557a0fd5e61158c
sly <atomicagression@gmail.com>
2021-05-26 15:45:44 +0300

    More

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(cors())
 var mysql = require('mysql');
 var pool  = mysql.createPool({
   connectionLimit : 10,
-  host            : '',
-  user            : '',
-  password        : '',
-  database        : ''
+  host            : 'source.dog',
+  user            : 'slydog_tests',
+  password        : 'slydog_tests',
+  database        : 'slydog_tests'
 });
 
 pool.query('SHOW DATABASES', function (error, results, fields) {
@@ -30,11 +30,27 @@ app.get('/', (req, res) => {
 });
 
 app.get('/test/:slug', (req, res) => {
-	pool.query(`SELECT * FROM tbl_tests LEFT JOIN tbl_questions ON tbl_tests.test_id = tbl_questions.parent_test WHERE tbl_tests.test_slug = '${req.params.slug}'`, function (error, results, fields) {
-	  if (error) throw error;
+  pool.query(`SELECT * FROM tbl_tests WHERE test_slug='${req.params.slug}'`, function (error, results, fields) {
+    if (error) throw error;
+
+    let test = results[0];
+
+    pool.query(`SELECT * FROM tbl_questions WHERE parent_test='${test.test_id}'`, function (error, results, fields) {
+      if (error) throw error;
+
+      test.questions = results;
+
+      res.json(test);
+    });
+  });
+});
+
+app.get('/options/:id', (req, res) => {
+  pool.query(`SELECT * FROM tbl_options WHERE parent_question='${req.params.id}'`, function (error, results, fields) {
+    if (error) throw error;
 
     res.json(results);
-	});
+  });
 });
  
 app.post('/', (req, res) => {
